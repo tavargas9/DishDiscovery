7af5f871ac4893d4dbdbb300973114a72b3fd0f6
Tomas Vargas <tavargas9@gmail.com>
2023-11-29 19:24:46 -0700

    modify dishRoutes

--- controllers/api/dishRoutes.js
+++ controllers/api/dishRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Dish } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newDish = await Dish.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newDish);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +17,19 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const dishData = await Dish.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!dishData) {
+      res.status(404).json({ message: 'No dish found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(dishData);
   } catch (err) {
     res.status(500).json(err);
   }

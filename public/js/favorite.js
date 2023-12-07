1d32a508867a1e98d97da31db97ab2c22a65c115
Cpulsipher24 <collintpulsipher@gmail.com>
2023-12-06 20:44:02 -0700

    Added logic to send favorites to server

--- public/js/favorite.js
+++ public/js/favorite.js
@@ -1,31 +1,34 @@
 const favoriteButton= document.querySelector("#favorite-button")
-
+favoriteButton.addEventListener("click", toggleFavorite)
 let isFavorite = false;
 
 function updateFavoriteButton(isFavorite) {
   const favoriteButton = document.getElementById('favorite-button');
   favoriteButton.textContent = isFavorite ? 'Remove from Favorites' : 'Save this recipe!';
+
 }
 
 function toggleFavorite() {
+    console.log("test")
   isFavorite = !isFavorite;
 
   updateFavoriteButton(isFavorite);
-
-  sendFavoriteStatusToServer(isFavorite);
+let recipeId= document.location.pathname.split("/")[3]
+  sendFavoriteStatusToServer(recipeId);
 }
 
-function sendFavoriteStatusToServer(isFavorite) {
-  fetch('/api/homeRoutes.js', {
-    method: 'POST',
+function sendFavoriteStatusToServer(recipeId) {
+  fetch('/api/dishes/favorite', {
+   method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ isFavorite }),
+    body: JSON.stringify({recipeId}),
   })
     .then(response => response.json())
     .then(data => {
       console.log('Favorite status updated successfully:', data);
+
     })
     .catch(error => {
       console.error('Error updating favorite status:', error);
